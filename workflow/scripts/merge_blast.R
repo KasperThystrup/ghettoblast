957b4c72d1716641af6cc2324e85c2535914e02e
Kasper Thystrup Karstensen <kathka@gmail.com>
2022-12-09 12:51:35 +0100

    Updated documentation
    Updated temporary debug file to be generated in the outdir and updated the documentation. Added a template config and blacklist file.

--- workflow/scripts/merge_blast.R
+++ workflow/scripts/merge_blast.R
@@ -158,10 +158,10 @@ merge_blast <- function(blast, faidx, blast_columns, metadata_file, exclude_seqs
   message("Done")
 }
 
-dn <- file.path(getwd(), "tmp")
-dir.create(dn)
-message("DEBUG: Saving image to ", fn <- file.path(dn, "merge_blast.RData"))
-save.image(file = fn)
+tmp_dir <- file.path(snakemake@params[["outdir"]], "tmp")
+dir.create(tmp_dir)
+message("DEBUG: Saving image to ", tmp_file <- file.path(tmp_dir, "merge_blast.RData"))
+save.image(file = tmp_file)
 
 message(paste("INFO: Merging", length(snakemake@input[["blast"]]), "samples"))
 
